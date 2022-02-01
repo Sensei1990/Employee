c7390451d02c71f277d634ab19a0d10c47c32517
Sensei1990 <pojar_alexandru90@yahoo.com>
2022-02-01 12:40:36 +0200

    New Classes, New projet, Just an Example

---
+++ src/EmployeeTest.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class EmployeeTest {
+    public static void main(String [] args){
+        Employee empOne = new Employee("Don Jon");
+        Employee empTwo = new Employee("Sarah James");
+
+        empOne.empAge(31);
+        empOne.empDesig("Senior Java Developer");
+        empOne.empSalary(2500);
+        empOne.printEmployee();
+
+        empTwo.empAge(25);
+        empTwo.empDesig("Middle Java Developer");
+        empTwo.empSalary(1500);
+        empTwo.printEmployee();
+    }
+}
